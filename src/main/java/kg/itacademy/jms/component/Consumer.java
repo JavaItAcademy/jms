4834d26aae05c5971a1b54ae48228e5e985eba1b
Askhat <serikov.askhat@gmail.com>
2020-05-28 21:39:56 +0600

    jms queue

---
+++ src/main/java/kg/itacademy/jms/component/Consumer.java
@@ -0,0 +1,13 @@
+package kg.itacademy.jms.component;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @JmsListener(destination = "my.queue")
+    public void consume(String message) {
+        System.out.println("Message received: " + message);
+    }
+}
